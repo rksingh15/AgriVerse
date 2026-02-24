a9cd52e608b02a6bf0c9ea3d1863cd4e6f33bc14
rksingh15 <ritiksinghkushwaha06@gmail.com>
2026-02-24 11:13:30 +0530

    Login Activity update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("com.google.android.material:material:1.12.0")
 }
